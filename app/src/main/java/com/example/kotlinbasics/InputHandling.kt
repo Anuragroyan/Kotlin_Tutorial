85d1c724a6e6e00ab62f45c736a5cc5102777a5e
Anurag Roy <38952781+Anuragroyan@users.noreply.github.com>
2024-03-14 21:06:21 +0530

    Demonstration of kotlin basics for coding like data types, variables, conditions  and conditional operators and user input from console etc

---
+++ app/src/main/java/com/example/kotlinbasics/InputHandling.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinbasics
+
+fun main(){
+    println("Please enter a number:")
+    var inputString = readln().toInt()
+    var inputNumber = inputString
+    val multipiler = 5
+    println(inputNumber*multipiler)
+}
